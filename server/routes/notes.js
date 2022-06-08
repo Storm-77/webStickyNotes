c39800481b700daae39abd01af9a336a13147db7
Storm-77 <czechu600@gmail.com>
2022-06-08 22:12:11 +0200

    jwt cookie

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { route } = require("express/lib/router");
 const mongodb = require("mongodb");
 const crypto = require('crypto')
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 //get notes
@@ -25,6 +26,14 @@ async function UsersCollection() {
     return null;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+function CreateToken(userId) {
+    return jwt.sign({ userId }, "super secret SEcret",
+        {
+            expiresIn: maxAge
+        });
+}
+
 router.get("/notes", async (req, res) => {
 
     res.json({
@@ -40,7 +49,7 @@ router.post("/register", async (req, res) => {
     if (!("password" in req.body && "name" in req.body)) {
         res.status(400);
         res.send("invalid object");
-        return ;
+        return;
     }
 
     const users = await UsersCollection();
@@ -50,7 +59,7 @@ router.post("/register", async (req, res) => {
         password: sha1(req.body.password)
     };
 
-    let result = await users.find(usr);
+    let result = await users.find({name:usr.name});
     let arr = await result.toArray();
 
     if (arr.length != 0) {
@@ -60,6 +69,8 @@ router.post("/register", async (req, res) => {
     }
 
     users.insertOne(usr).then(() => {
+         const token = CreateToken(usr._id);
+         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
 
         res.json({
             id: usr._id
@@ -67,18 +78,49 @@ router.post("/register", async (req, res) => {
     }).catch(e => {
         res.status(400);
         res.send("Cannot commit operation");
+        console.error(e);
     });
     //implement jwt
 });
 
 router.post("/login", async (req, res) => {
+    if (!("password" in req.body && "name" in req.body)) {
+        res.status(400);
+        res.json({ message: "invalid object" });
+        return;
+    }
+    const users = await UsersCollection();
+    let usr = {
+        name: req.body.name,
+        password: sha1(req.body.password)
+    };
+
+    let dbusr = await users.findOne(usr);
+
+    if (dbusr === null) {
+        res.status(404);
+        res.json({ message: "incorrect login" });
+        return;
+    }
+    res.json({
+        token: dbusr._id // TODO use a real token, not user id
+    });
 
 });
 
-//add note
+router.post("/notes", async (req, res) => {
+    //add note
 
-//delete note
+});
 
-//get all user's notes
+router.delete("/notes", async (req, res) => {
+    //delete note
+
+});
+
+router.get("/notes", async (req, res) => {
+    //get all user's notes
+
+});
 
 module.exports = router;
